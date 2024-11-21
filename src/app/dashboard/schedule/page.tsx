ef8d27bfe6281ce7a1eb5aee4ad8cd641cf1cc83
NathanielAddo <stevenelly100@gmail.com>
2024-11-21 07:56:01 +0000

    Add Attendance schedule pages

--- src/app/dashboard/schedule/page.tsx
+++ src/app/dashboard/schedule/page.tsx
@@ -4,6 +4,9 @@ import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 import UpdateSchedulePage from "@/components/schedule/viewSchedule";
 import CreateSchedule from "@/components/schedule/createSchedule";
+import BulkAttendanceScheduling from "@/components/schedule/bulkAttendanceScheduling";
+import IndividualAttendanceScheduling from "@/components/schedule/individualAttendanceScheduling";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const SchedulePage: React.FC = () => {
   const [showCreateSchedule, setShowCreateSchedule] = useState(false);
@@ -15,17 +18,30 @@ const SchedulePage: React.FC = () => {
   return (
     <div className="container w-full">
       <div className="w-full flex justify-between items-center">
-        <h1 className="text-2xl font-bold mb-4">Create/Update Schedule</h1>
+        <h1 className="text-2xl font-bold mb-4">Attendance Scheduling</h1>
         <Button
           onClick={handleShow}
           variant="default"
-          className="bg-ds-primary text-ds-foreground hover:bg-ds-primary-dark"
+          className="bg-ds-primary text-ds-foreground"
         >
           {showCreateSchedule ? "Close Schedule" : "Add Schedule"}
         </Button>
       </div>
 
-      {showCreateSchedule && <CreateSchedule onClose={handleShow} />}
+      {showCreateSchedule && (
+        <Tabs defaultValue="individual" className="w-full">
+          <TabsList>
+            <TabsTrigger value="individual">Individual Scheduling</TabsTrigger>
+            <TabsTrigger value="bulk">Bulk Scheduling</TabsTrigger>
+          </TabsList>
+          <TabsContent value="individual">
+            <IndividualAttendanceScheduling onClose={handleShow} />
+          </TabsContent>
+          <TabsContent value="bulk">
+            <BulkAttendanceScheduling onClose={handleShow} />
+          </TabsContent>
+        </Tabs>
+      )}
 
       <UpdateSchedulePage />
     </div>
@@ -33,3 +49,4 @@ const SchedulePage: React.FC = () => {
 };
 
 export default SchedulePage;
+
